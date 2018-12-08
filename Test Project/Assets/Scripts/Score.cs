0cebbd738d45d47367650af8b237ed311838fca7
andro951 <andro951@gmail.com>
2018-12-08 17:22:33 -0500

    Revert "Revert "Discovered new problem.  Makes infinite starShips.  Reverting to "Fixed Issue: starShiw was being declaired as a new local object instead of changing starShip defined in the header."""
    This reverts commit 325a7f94495731e6fe30f1d0e5437a22de990fad.

--- Test Project/Assets/Scripts/Score.cs
+++ Test Project/Assets/Scripts/Score.cs
@@ -5,12 +5,14 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour {
     public Text scoreText;
-	// Use this for initialization
-	void Start () {
+	
+	void Start ()
+    {
 		
 	}
-	// Update is called once per frame
-	void Update () {
-        scoreText.text = "Score: " + GameManager.instance.score;
+	
+	void Update ()
+    {
+        scoreText.text = "Score: " + GameManager.instance.score; //Display the Player's score
     }
 }
